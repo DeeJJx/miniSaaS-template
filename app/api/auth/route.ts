391d61ce959a56ac0b0f103b6a9c392609315c51
DeeJJx <james.johnson@graduate.org>
2024-08-19 21:24:57 +0100

    update to avoid api route issue

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,6 @@
 // // /app/api/auth/route.ts
 
-// import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 // import NextAuth from 'next-auth';
 // import Providers from 'next-auth/providers/index';
 // // import CredentialsProvider from 'next-auth/providers/credentials'
@@ -16,7 +16,11 @@
 //   });
 // }
 
-// export function POST(request: NextRequest) {
-//   // Handle POST request for authentication
-//   return NextResponse.json({ message: 'Auth route' });
-// }
+export function POST(request: NextRequest) {
+  // Handle POST request for authentication
+  return NextResponse.json({ message: 'Auth route' });
+}
+
+// export default function handler(req: any, res: any) {
+//     res.status(200).json({ message: "Hello from the API route!" });
+//   }
