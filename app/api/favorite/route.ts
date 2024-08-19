5ab2cafa9588b6e86d688f6681fc87cd3cf520f5
DeeJJx <james.johnson@graduate.org>
2024-08-19 21:39:18 +0100

    update more routes

--- app/api/favorite/route.ts
+++ app/api/favorite/route.ts
@@ -6,12 +6,14 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
-  const { gameId, userId } = await request.json();
+  // const { gameId, userId } = await request.json();
 
-  const favorite = await prisma.game.update({
-    where: { id: gameId },
-    data: { user: { connect: { id: userId } } },
-  });
+  // const favorite = await prisma.game.update({
+  //   where: { id: gameId },
+  //   data: { user: { connect: { id: userId } } },
+  // });
 
-  return NextResponse.json(favorite);
+  // return NextResponse.json(favorite);
+  return NextResponse.json({ message: 'Favourite route' });
+
 }
--- app/api/submit-game/route.ts
+++ app/api/submit-game/route.ts
@@ -6,14 +6,16 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export async function POST(request: NextRequest) {
-  const { title, userId } = await request.json();
+  // const { title, userId } = await request.json();
 
-  const game = await prisma.game.create({
-    data: {
-      title,
-      user: { connect: { id: userId } },
-    },
-  });
+  // const game = await prisma.game.create({
+  //   data: {
+  //     title,
+  //     user: { connect: { id: userId } },
+  //   },
+  // });
 
-  return NextResponse.json(game);
+  // return NextResponse.json(game);
+  return NextResponse.json({ message: 'Submit game route' });
+
 }
