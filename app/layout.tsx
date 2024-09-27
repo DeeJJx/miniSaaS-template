139b4b23376ef580b3efbfa2b2fbaa15bcf046ca
DeeJJx <james.johnson@graduate.org>
2024-09-27 14:55:52 +0100

    update crossOrigin

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import { Provider } from "./provider";
 import Header from "./components/Header";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,8 +20,8 @@ export default function RootLayout({
   return (
     <html lang="en">
           <head>
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7774265661621647"
-          crossorigin="anonymous"></script>
+          <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7774265661621647"
+          crossOrigin="anonymous"></Script>
           </head>
           <body className={inter.className}>
             <Provider>
