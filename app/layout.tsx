80dd1f62406b1f5d1c62c05fbfaad6c34127f79a
DeeJJx <james.johnson@graduate.org>
2024-09-27 14:51:03 +0100

    adsense script

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+          <head>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7774265661621647"
+          crossorigin="anonymous"></script>
+          </head>
           <body className={inter.className}>
             <Provider>
               <Header />
